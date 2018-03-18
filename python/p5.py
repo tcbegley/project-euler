2d57bea2fcaa77039b962cf35fbfc4ee1fa9b26e
tcbegley <tom@asidatascience.com>
2018-03-18 09:59:16 +0000

    Python solution of problem 5

---
+++ python/p5.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    """
+    Writing a program for this is a little unnecessary, the answer is simply
+    the product of prime powers less than 20.
+    """
+    print(16 * 9 * 5 * 7 * 11 * 13 * 17 * 19)
