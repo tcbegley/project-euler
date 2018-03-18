2ab8133bb79203a94c6a3cbdf67684da9d9cddc7
tcbegley <tom@asidatascience.com>
2018-03-18 21:43:46 +0000

    Adding python solutions for problems 18 and 67
    The problems are the same, one with more data than the other.

---
+++ python/p18.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    with open("../data/p18.txt", "r") as f:
+        tree = [
+            [int(x) for x in row.strip().split(' ')]
+            for row in f.read().strip().split('\n')
+        ]
+
+    for i in range(len(tree)-2, -1, -1):
+        for j in range(len(tree[i])):
+            tree[i][j] += max(tree[i+1][j], tree[i+1][j+1])
+
+    print(tree[0][0])
